a1af461e6ab5c5ad704db2d004218725542ffe7b
DESKTOP-B67THC4\sinik <sini.karhunen@gmail.com>
2021-01-24 17:57:39 +0200

    ..

--- LoopDeLoop/Assets/Scripts/SininSkriptit/CameraScaler.cs
+++ LoopDeLoop/Assets/Scripts/SininSkriptit/CameraScaler.cs
@@ -8,48 +8,33 @@ public class CameraScaler : MonoBehaviour
     public float screenWidth = 1080f;
     public float targetAspect = 9f / 16f;
     public float orthographicSize;
-    private Camera mainCamera;
+    private Camera camera;
 
-    void Start()
-    {
-        // set the desired aspect ratio (the values in this example are
-        // hard-coded for 16:9, but you could make them into public
-        // variables instead so you can set them at design time)
-        float targetaspect = 9.0f / 16.0f;
+    void Start() {
 
-        // determine the game window's current aspect ratio
-        float windowaspect = (float)Screen.width / (float)Screen.height;
-
-        // current viewport height should be scaled by this amount
-        float scaleheight = windowaspect / targetaspect;
 
         // obtain camera component so we can modify its viewport
         Camera camera = GetComponent<Camera>();
+    }
+     private float DesignOrthographicSize;
+    private float DesignAspect;
+    private float DesignWidth;
 
-        // if scaled height is less than current height, add letterbox
-        if (scaleheight < 1.0f) {
-            Rect rect = camera.rect;
+    public float DesignAspectHeight;
+    public float DesignAspectWidth;
 
-            rect.width = 1.0f;
-            rect.height = scaleheight;
-            rect.x = 0;
-            rect.y = (1.0f - scaleheight) / 2.0f;
+    public void Awake() {
+        this.DesignOrthographicSize = this.camera.orthographicSize;
+        this.DesignAspect = this.DesignAspectHeight / this.DesignAspectWidth;
+        this.DesignWidth = this.DesignOrthographicSize * this.DesignAspect;
 
-            camera.rect = rect;
-        } else // add pillarbox
-          {
-            float scalewidth = 1.0f / scaleheight;
-
-            Rect rect = camera.rect;
-
-            rect.width = scalewidth;
-            rect.height = 1.0f;
-            rect.x = (1.0f - scalewidth) / 2.0f;
-            rect.y = 0;
-
-            camera.rect = rect;
-        }
+        this.Resize();
     }
 
+    public void Resize() {
+        float wantedSize = this.DesignWidth / this.camera.aspect;
+        this.camera.orthographicSize = Mathf.Max(wantedSize,
+            this.DesignOrthographicSize);
+    }
 
 }
